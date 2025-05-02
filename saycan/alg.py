87d455482ed85b24e50029d847256707f54d72b1
Rushang Karia <Rushang.Karia@asu.edu>
2025-05-01 21:23:27 -0700

    Run saycan end-to-end example.

--- saycan/alg.py
+++ saycan/alg.py
@@ -4,27 +4,20 @@ from saycan import prompt
 from saycan import llm
 from saycan import vild
 from saycan import constants
-from saycan import prompt
 from saycan import helper
 import numpy as np
 import time
-import shutil
 import os
 import tensorflow.compat.v1 as tf
+import tempfile
 
 class SayCan:
 
     SAVED_MODEL_DIRPATH = "%s/image_path_v2" % (saycan.ASSETS_DIR)
 
-    def __init__(self, env, model_dirpath=SAVED_MODEL_DIRPATH,
-        output_dir=saycan.OUTPUT_DIR, clean=True):
-
-        if clean:
-            shutil.rmtree(output_dir, ignore_errors=True)
-        os.makedirs(output_dir, exist_ok=True)
+    def __init__(self, env, model_dirpath=SAVED_MODEL_DIRPATH):
 
         self.env = env
-        self.output_dir = output_dir
 
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2)
         self.session = tf.Session(graph=tf.Graph(), config=tf.ConfigProto(
@@ -35,23 +28,24 @@ class SayCan:
             constants.PLACE_TARGETS,
             termination_string=prompt.TERMINATION_STRING)
 
-        if saycan.VERBOSITY >= 2:
+        self.optimizer, num_params = vild.get_vild_optimizer()
+        self.scene_description = None
+        self.found_objects = None
+        self.affordance_scores = None
+
+        if saycan.VERBOSITY:
             print("SayCan is considering", len(self.actions), "actions")
-
-        self.env_description, self.found_objects = self._get_env_description()
-
-        self.affordance_scores = helper.affordance_scoring(self.actions,
-            self.found_objects, block_name="box", bowl_name="circle",
-            verbose=False)
-
-        self.task_no = 0
+            print(f'ViLD parameters: {num_params:,}')
     
-    def _get_env_description(self):
+    def initialize_vild_and_compute_affordances(self, output_dir):
 
-        image_path = "%s/env_init_image.png" % (self.output_dir)
+        if self.found_objects is not None:
+            return
+
+        image_path = "%s/vild_object_detect.png" % (output_dir)
         helper.save_env_top_image(self.env, image_path, show=False)
 
-        found_objects, scores = vild.vild(self.session, image_path, 
+        self.found_objects, scores = vild.vild(self.session, image_path, 
             constants.category_name_string, constants.vild_params,
             plot_on=False)
 
@@ -60,19 +54,27 @@ class SayCan:
             for found_object, score in zip(found_objects, scores):
                 print("Found a", found_object, "with score:", score)
 
-        scene_description = helper.build_scene_description(found_objects)
+        self.scene_description = helper.build_scene_description(found_objects)
 
         if saycan.VERBOSITY >= 2:
             print("SayCan scene description")
             print(scene_description)
 
-        return scene_description, found_objects
+        self.affordance_scores = helper.affordance_scoring(self.actions,
+            self.found_objects, block_name="box", bowl_name="circle",
+            verbose=False)
 
-    def saycan(self, task_prompt, client, max_horizon=10):
+    def saycan(self, output_dir, task_prompt, client, state=None,
+        max_horizon=10):
         
-        task_dir = "%s/task%u" % (self.output_dir, self.task_no)
-        os.makedirs(task_dir, exist_ok=True)
-        print("Saving data to:", task_dir)
+        os.makedirs(output_dir, exist_ok=True)
+        self.initialize_vild_and_compute_affordances(output_dir)
+
+        if state is None:
+            self.env.set_state(self.env.init_state_id)
+
+        image_path = "%s/saycan_init_s.png" % (output_dir)
+        helper.save_env_top_image(self.env, image_path, show=False)
 
         high_level_actions = []
         selected_action = ""
@@ -80,7 +82,7 @@ class SayCan:
         while selected_action != task_prompt.termination_string \
             and h < max_horizon:
 
-            with open("%s/prompt-%u.txt" % (task_dir, h), "w") as fh:
+            with open("%s/prompt-%u.txt" % (output_dir, h), "w") as fh:
                 fh.write(str(task_prompt))
 
             llm_scores = client.get_scores(task_prompt, self.actions)
@@ -97,19 +99,49 @@ class SayCan:
             task_prompt.append(selected_action, add_separator=True)
             h += 1
 
-            fig_filepath = "%s/%s.png" % (task_dir, selected_action)
+            fig_filepath = "%s/%s.png" % (output_dir, selected_action)
             helper.plot(llm_scores, self.affordance_scores, saycan_scores,
                 selected_action, fig_filepath=fig_filepath)
 
-        self.task_no += 1
         return high_level_actions
 
-    def execute_plan(self, high_level_actions):
+    def execute_plan(self, output_dir, plan,
+        state=None, store_video=True):
 
-        pass
+        os.makedirs(output_dir, exist_ok=True)
 
+        with open("%s/plan.txt" % (output_dir), "w") as fh:
+            fh.write("\n".join(plan))
+
+        if state is None:
+            self.env.set_state(self.env.init_state_id)
+        
+        steps = []
+        for i, action in enumerate(plan):
+
+            if action == prompt.TERMINATION_STRING:
+                break
+
+            steps.append(i)
+            step_dir = "%s/step-%u_%s" % (output_dir, i, action)
+            os.makedirs(step_dir, exist_ok=True)
+            
+            obs = self.env.get_state()
+            nlp_action = helper.convert_action_to_cliport_action(action)
+            vild.run_cliport(step_dir, self.optimizer, self.env, obs,
+                nlp_action)
+
+            if store_video:
+                step_video_filepath = "%s/step_execution.mp4" % (step_dir)
+                self.env.save_video(step_video_filepath, [i])
+
+        if store_video:
+            execution_filepath = "%s/plan_execution.mp4" % (output_dir)
+            self.env.save_video(execution_filepath,  range(len(steps)))
 
 if __name__ == "__main__":
+
+    import shutil
 
     print("SayCan Algorithm")
 
@@ -133,12 +165,19 @@ if __name__ == "__main__":
 
     env = PickPlaceEnv(TASK_CONFIG)
     _ = env.reset()
-    saycan = SayCan(env, clean=False)
+    saycan_obj = SayCan(env)
 
     import openai
     api_key = "invalid"
     base_url = "http://en4230849l.scai.dhcp.asu.edu:8000/v1"
     MODEL_NAME = "meta-llama/Llama-3.1-8B"
+    OUTPUT_DIR = "/tmp/results"
+    CLEAN = True
+
+    if CLEAN:
+        shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     from saycan.llm import SayCanLLMClient
     from saycan.prompt import Prompt
@@ -146,6 +185,15 @@ if __name__ == "__main__":
     client = SayCanLLMClient(MODEL_NAME, api_key=api_key,
         base_url=base_url)
 
-    prompt = Prompt(TASK)
-    saycan.saycan(prompt, client)
+    task_prompt = Prompt(TASK)
+    plan = saycan_obj.saycan(OUTPUT_DIR, task_prompt, client)
+
+    # DUMMY_PLAN = ["robot.pick_and_place(green block, middle)",
+    #     "robot.pick_and_place(yellow block, green block)",
+    #     "robot.pick_and_place(red block, yellow block)",
+    #     "done()"
+    # ]
+    # plan = DUMMY_PLAN
+
+    saycan_obj.execute_plan(OUTPUT_DIR, plan, "/tmp/results")
 
